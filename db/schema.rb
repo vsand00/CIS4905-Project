922ba25a0fad332a2c10bf1d179ef12adef63b85
vsand00 <veronicaisandoval@hotmail.com>
2020-12-08 15:19:19 -0500

    Postgres successful switch

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_10_29_151527) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.text "commenter"
     t.text "body"
